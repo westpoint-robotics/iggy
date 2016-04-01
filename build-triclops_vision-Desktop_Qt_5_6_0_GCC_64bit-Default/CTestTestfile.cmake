d65374d21271bcab35052c8fe9e69146cb9af1d5
user1igvc2 <user1@igvc2.com>
2016-04-01 15:38:22 -0400

    working triclops code and pointcloud

--- build-triclops_vision-Desktop_Qt_5_6_0_GCC_64bit-Default/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user1/catkin_ws/src/triclops_vision
-# Build directory: /home/user1/catkin_ws/src/build-triclops_vision-Desktop_Qt_5_6_0_GCC_64bit-Default
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
