ae0b88e4741e729163a2671f9863365e0358e3bc
user1igvc2 <user1@igvc2.com>
2017-03-29 14:07:48 -0400

    some changes on clinton lott

---
+++ igvc_run/src/marker_pub.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+import csv
+import roslib
+import rospy
+from visualization_msgs.msg import Marker
+from geometry_msgs.msg import Pose
+
+def make_waypoint_viz(pos,txt,ident):
+    marker = Marker()
+    marker.header.frame_id = "map"
+    marker.header.stamp = rospy.Time.now()
+    marker.ns = "my_namespace"
+    marker.id = ident
+    #marker.type = Marker.SPHERE
+    marker.type = Marker.TEXT_VIEW_FACING
+    marker.text = str(ident)
+    #marker.lifetime = 30.0
+    marker.action = Marker.ADD
+    marker.pose = pos
+    #marker.pose.position.x = 1
+    #marker.pose.position.y = 1
+    #marker.pose.position.z = 1
+    #marker.pose.orientation.x = 0.0
+    #marker.pose.orientation.y = 0.0
+    #marker.pose.orientation.z = 0.0
+    #marker.pose.orientation.w = 1.0
+    marker.scale.x = 2.0
+    marker.scale.y = 2.0
+    marker.scale.z = 2.0
+    marker.color.a = 1.0 # Don't forget to set the alpha!
+    marker.color.r = 1.0
+    marker.color.g = 0.0
+    marker.color.b = 1.0
+    #only if using a MESH_RESOURCE marker type:
+    #marker.mesh_resource = "package://pr2_description/meshes/base_v0/base.dae"
+    return marker
+
+#Test Code below
+if __name__ == '__main__':
+    rospy.init_node('waypoint_visualization', anonymous=True)
+    vis_pub = rospy.Publisher( "visualization_marker", Marker, queue_size=1 )
+    rate = rospy.Rate(1)
+    p = Pose()
+    x=0
+    while not rospy.is_shutdown():
+        t = "HERENOW!"
+        p.position.x = 1+x
+        p.position.y = 1
+        p.position.z = 0
+        p.orientation.x = 0.0
+        p.orientation.y = 0.0
+        p.orientation.z = 0.0
+        p.orientation.w = 1.0    
+        vis_pub.publish( make_waypoint_viz(p,t,x ))
+        x +=1
+        rate.sleep()
+
+
