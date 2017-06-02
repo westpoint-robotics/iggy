d74c19062b25fb1f38d7cd97c5f93d4b4d517496
user1igvc2 <user1@igvc2.com>
2017-06-02 12:59:39 -0400

    completed avoidance tests

--- usma_roboteq/src/roboteq_driver_2017.py
+++ usma_roboteq/src/roboteq_driver_2017.py
@@ -4,6 +4,8 @@
 The Roboteq Controller has a script running on it that will enable and disable the ESTOP with no
 interaction from this code. The ESTOP and manual driving modes are not effected by this script, they
 are independent of Iggy's computer.
+
+
 SYMBOLS USED FOR SERIAL COMMO:
 ~ Read configuration operation 
 ^ Write configuration operation 
@@ -11,32 +13,30 @@ SYMBOLS USED FOR SERIAL COMMO:
 ? Run time queries
 % Maintenance Commans
 _ Carriage return
+
 + Command acknowledgement
 - Command not recognized or accepted
+
 '''
 import serial
 import rospy
 import time
 import os
 import sys
+
 from std_msgs.msg import Bool
 from std_msgs.msg import Float32
 from geometry_msgs.msg import Twist
-
 from geometry_msgs.msg import Vector3
 
-# global variables
-controlMode = 0 # Mode 0 = Estop; Mode 1 = Manual; Mode 2 = Autonomous
+cmd_vel = Twist()
 
-# Create a log file
-#outFile = open("roboteqLog.txt", "wb")
-
-# While data is in the buffer read it, then return it.
+# While data is in the serial buffer read it, then return it.
 def getdata():
     info = ''
     while ser.inWaiting() > 0: 
         char = ser.read()
-        #if char != '\r':
+        #if char != '\r': 
         info += str(char)
     #print "INFO: ",info
     #if len(info) == 7:
@@ -58,58 +58,10 @@ def getBattVoltage():
         #rospy.loginfo( "ERROR: ROBOTEQ Error in getBattVoltage: %s", e )
     return -999.999 # an error has occured and we return
 
-# Updates the global variable for controlMode
-def setControlMode():
-    global controlMode # use the global controlMode
-    try:
-        getdata()   #clear buffer
-        ser.write('?PI 4\r') #pulse input of pulse-in 4 (switch - up)
-        time.sleep(.005)
-        pi4 = getdata()
-        ser.write('?PI 5\r') #pulse input of pulse-in 5 (switch - down)
-        time.sleep(.005)
-        pi5 = getdata()
-        if len(pi4) == len(pi5) == 8: # process only if data is valid
-            tot = int(pi4[3:7]) + int(pi5[3:7])
-            #rospy.loginfo("ROBOTEQ Controller in mode:%d tot is:%d",controlMode,tot)  
-            if tot > 3500:
-                controlMode = 0
-            elif tot < 2500:  
-                controlMode = 2
-            else:
-                controlMode = 1             
-        else:
-            #print ("ROBOTEQ DRIVER PWM for Pin 4 an 5 not LEN of 8 4:%d 5:%d\n",len(pi4),len(pi5))
-            pass
-    except (KeyboardInterrupt, SystemExit):
-        raise
-    except: # catch *all other* exceptions
-        e = sys.exc_info()[0]
-        rospy.loginfo( "<p>ROBOTEQ Error in setControlMode: %s</p>", e )
-
-def sendStop():
-        cmd = '!G 1 0\r'
-        ser.write(cmd)
-        cmd = '!G 2 0\r'
-        ser.write(cmd)
-
-def moveCallback(data):
-    global controlMode
-    if (controlMode == 2):  # robot is in autonomous mode
-        #rospy.loginfo("ROBOTEQCB Controller inside if mode:%d",controlMode)  
-        speed = data.linear.x *-1000 #linear.x is value between -1 and 1 and input to wheels is between -1000 and 1000
-                                    #1000 would give full speed range, but setting to lower value to better control robot
-        turn = (data.angular.z + 0.009)*500 
-        # G - Go to Speed or to Relative Position
-        #print speed,turn
-        cmd = '!G 1 ' + str(speed) + '\r'
-        ser.write(cmd)
-        cmd = '!G 2 ' + str(turn) + '\r'
-        ser.write(cmd)
-
 # Configures the roboteq motor controller to work with Iggy. Relying on the EEPROM has proven unreliable.
 def initalizeController():    
-    ser.write('^ECHOF 1\r') # Turn on the ESTOP while resetting the configuration for the roboteq device
+    ser.write('^ECHOF 1\r') # Turn off echo commands
+    ser.write('!EX\r') # TTurn on the ESTOP while resetting the configuration for the roboteq device 
     time.sleep(.01)
     result = getdata()   
     ser.write('!R 0\r') # Stop any scripts running on the roboteq device
@@ -123,13 +75,12 @@ def initalizeController():
     for cmd in configCmds:
         result = getdata()
         ser.write(cmd)
-        time.sleep(.1)
+        time.sleep(.01)
         result = getdata()
-        rospy.loginfo("CONFIG FEEDBACK : %s " %result)
         if (result != '+\r'): # If setting the configuration fails
             ser.write('!E 1\r') # Turn on the ESTOP
             ser.write('!R 0\r') # Stop any scripts
-            rospy.loginfo("ERROR: ROBOTEQ DRIVER FAILED TO SET CONFIGURATION WITH: %s \n" %cmd)
+            rospy.loginfo("ERROR: ROBOTEQ DRIVER FAILED TO SET CONFIGURATION WITH: %s \n",  cmd)
             rospy.loginfo("ERROR: ROBOTEQ DRIVER CONFIGURATION FAILED. NOW EXITING ROBOTEQ DRIVER\n\n")
             exit()
 
@@ -137,7 +88,41 @@ def initalizeController():
     time.sleep(.01)
     result = getdata()
     rospy.loginfo("SUCCESSFULLY CONFIGURED THE ROBOTEQ MOTOR CONTROLLER\n")
-    
+
+# Updates the global variable for current_mode
+def getControlMode():
+    mode =0
+    try:
+        getdata()   #clear buffer
+        ser.write('?PI 4\r') #pulse input of pulse-in 4 (switch - up)
+        time.sleep(.005)
+        pi4 = getdata()
+        ser.write('?PI 5\r') #pulse input of pulse-in 5 (switch - down)
+        time.sleep(.005)
+        pi5 = getdata()
+        if len(pi4) == len(pi5) == 8: # process only if data is valid
+            tot = int(pi4[3:7]) + int(pi5[3:7])
+            #rospy.loginfo("ROBOTEQ Controller in mode:%d tot is:%d",mode,tot)  
+            if tot > 3500:
+                mode = 0
+            elif tot < 2500:  
+                mode = 2
+            else:
+                mode = 1             
+        else:
+            #print ("ROBOTEQ DRIVER PWM for Pin 4 an 5 not LEN of 8 4:%d 5:%d\n",len(pi4),len(pi5))
+            pass
+    except (KeyboardInterrupt, SystemExit):
+        raise
+    except: # catch *all other* exceptions
+        e = sys.exc_info()[0]
+        rospy.loginfo( "<p>ROBOTEQ Error in getControlMode: %s</p>", e )
+    return mode
+
+def moveCallback(data):
+    global cmd_vel
+    cmd_vel = data
+
 if __name__ == "__main__":
 
     # configure the serial connections 
@@ -176,21 +161,66 @@ if __name__ == "__main__":
     initalizeController()
 
     rate = rospy.Rate(30)
-    lastControlMode = controlMode
+    last_mode = 0 #set to e-stop mode
     while not rospy.is_shutdown():
-        setControlMode()
-        #rospy.loginfo("ROBOTEQ Controller in mode:%d",controlMode)  
+        current_mode = getControlMode() # set control mode based on the controller switch location. 
         battVolt = getBattVoltage()
         if battVolt > 0.01:            
             volt_pub.publish(Float32(battVolt))
-        if (controlMode == 2):  #robot is in autonomous mode
+
+        #State machine below here
+        # Mode 0 = Estop; Mode 1 = Manual; Mode 2 = Autonomous
+
+        # If transitioning from e-stop mode then release e-stop
+        if (last_mode == 0 & current_mode != 0): 
+            ser.write('!MG 0\r') # Release the ESTOP 
+            time.sleep(.01)
+            result = getdata()
+
+        # If transitioning from autonomous mode then stop autonomous drive commands
+        if (last_mode == 2 & current_mode != 2): 
+            cmd = '!G 1 0\r'
+            ser.write(cmd)
+            cmd = '!G 2 0\r'
+            ser.write(cmd)
+            time.sleep(.01)
+            result = getdata()
+
+        #rospy.loginfo("ROBOTEQ Controller in mode:%d",current_mode) 
+        if (current_mode == 2):  #robot is in AUTONOMOUS MODE            
             auto_pub.publish(True) # Turn on autonomous lights
-        else:    
-            if(lastControlMode == 2):
-                sendStop()
-            auto_pub.publish(False)
-        lastControlMode = controlMode
+            speed = cmd_vel.linear.x *-1000 #linear.x is value between -1 and 1 and input to wheels is between -1000 and 1000
+                                        #1000 would give full speed range, but setting to lower value to better control robot
+            turn = (cmd_vel.angular.z + 0.009)*500 
+            # G - Go to Speed or to Relative Position
+            #print speed,turn
+            cmd = '!G 1 ' + str(speed) + '\r'
+            ser.write(cmd)
+            cmd = '!G 2 ' + str(turn) + '\r'
+            ser.write(cmd)
+
+        elif (current_mode == 1): # MANUAL MODE
+            auto_pub.publish(False) # Turn off autonomous lights
+            # This code sends no more commands to roboteq in this mode 
+            # commands are sent directly from Traxxis remote to the Roboteq Controller
+
+        else: # E-STOP MODE
+            auto_pub.publish(False) # Turn off autonomous lights
+            #TODO check if in estop mode. if not then put it in it.
+            if (last_mode != 0 & current_mode == 0): 
+                ser.write('!EX\r') # Turn on the ESTOP 
+                time.sleep(.01)
+                result = getdata() 
+
+        last_mode = current_mode
         rate.sleep()
 
-    ser.close()
+
+
+
+
+
+
+
+
 
